338fbbae9a991b3b2ebac9e7632621c3f1482c9a
head_monitor <hectorangelopoulos@gmail.com>
2023-04-06 17:54:31 +0300

    Continued work on ChatItem

--- src/main/java/net/nexia/nexiacollection/Modules/ChatItem.java
+++ src/main/java/net/nexia/nexiacollection/Modules/ChatItem.java
@@ -1,5 +1,11 @@
 package net.nexia.nexiacollection.Modules;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Content;
+import net.md_5.bungee.api.chat.hover.content.Text;
+import net.nexia.nexiaapi.Processes;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +14,8 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public class ChatItem implements Listener
 {
@@ -29,7 +37,27 @@ public class ChatItem implements Listener
 
         if (message.contains("[item]"))
         {
-            e.setMessage(message.replace("[item]", "[" + itemMeta.getDisplayName() + "]"));
+            TextComponent itemMessage = new TextComponent("[" + itemName +"]");
+            List<String> lore = itemMeta.getLore();
+            int loreSize = lore == null ? 0 : lore.size();
+
+            Content[] hoverEventComponents = new Content[loreSize + 2];
+            hoverEventComponents[0] = new Text(itemName + "\n");
+
+            for (int i = 1; i < hoverEventComponents.length; i++)
+                hoverEventComponents[i] = new Text("");
+
+            if (itemMeta.getLore() != null)
+            {
+                for (int i = 0; i < loreSize; i++)
+                    hoverEventComponents[i + 1] = new Text(lore.get(i) + "\n");
+            }
+
+            itemMessage.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverEventComponents));
+
+            e.setCancelled(true);
+            for (Player p : Bukkit.getOnlinePlayers())
+                p.spigot().sendMessage(itemMessage);
         }
     }
 
